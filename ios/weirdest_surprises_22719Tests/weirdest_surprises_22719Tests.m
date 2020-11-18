f2351f90cfa0cba0b599cc2c555230b8b3b39867
Crowdbotics <team@crowdbotics.com>
2020-11-18 03:52:26 +0000

    Initial commit.

---
+++ ios/weirdest_surprises_22719Tests/weirdest_surprises_22719Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface weirdest_surprises_22719 : XCTestCase
+
+@end
+
+@implementation weirdest_surprises_22719
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
